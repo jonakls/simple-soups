2ec3b717c18ddf93f3eba3fa7311425371084641
xGardenDev <asmot54@hotmail.com>
2021-09-17 21:05:58 -0500

    Changed data of maps to uuid players

--- src/main/java/me/gardendev/simplesoups/storage/PlayerCache.java
+++ src/main/java/me/gardendev/simplesoups/storage/PlayerCache.java
@@ -10,11 +10,11 @@ public class PlayerCache {
     private final DataStorage storage;
 
     private final List<Player> players;
-    private final Map<String, String> id;
-    private final Map<String, Integer> kills;
-    private final Map<String, Integer> deaths;
-    private final Map<String, Integer> xp;
-    private final Map<String, List<String>> kits;
+    private final Map<UUID, UUID> id;
+    private final Map<UUID, Integer> kills;
+    private final Map<UUID, Integer> deaths;
+    private final Map<UUID, Integer> xp;
+    private final Map<UUID, List<String>> kits;
 
     public PlayerCache(PluginCore pluginCore) {
         this.storage = pluginCore.getStorage();
@@ -30,89 +30,89 @@ public class PlayerCache {
         for (Player player : players) {
             this.storage.insert(this, player);
             players.remove(player);
-            kills.remove(player.getName());
+            kills.remove(player.getUniqueId());
         }
     }
 
     public void savePlayerData(Player player) {
         this.storage.insert(this, player);
         this.players.remove(player);
-        this.kills.remove(player.getName());
+        this.kills.remove(player.getUniqueId());
 
     }
 
     public void loadPlayerData(Player player) {
         players.add(player);
-        this.kills.put(player.getName(), storage.getKills(player.getUniqueId().toString()));
-        this.deaths.put(player.getName(), storage.getDeaths(player.getUniqueId().toString()));
-        this.xp.put(player.getName(), storage.getXp(player.getUniqueId().toString()));
+        this.kills.put(player.getUniqueId(), storage.getKills(player.getUniqueId().toString()));
+        this.deaths.put(player.getUniqueId(), storage.getDeaths(player.getUniqueId().toString()));
+        this.xp.put(player.getUniqueId(), storage.getXp(player.getUniqueId().toString()));
     }
 
     public void registerPlayer(Player player) {
         if (players.contains(player)) return;
         players.add(player);
-        id.put(player.getName(), player.getUniqueId().toString());
+        id.put(player.getUniqueId(), player.getUniqueId());
     }
 
     public void incrementKills(Player player) {
-        Integer killsValue = this.kills.get(player.getName());
+        Integer killsValue = this.kills.get(player.getUniqueId());
         if(killsValue == null) {
-            this.kills.put(player.getName(), 0);
+            this.kills.put(player.getUniqueId(), 0);
             killsValue = 0;
         }
-        this.kills.put(player.getName(), killsValue + 1 );
+        this.kills.put(player.getUniqueId(), killsValue + 1 );
     }
 
     public void incrementDeaths(Player player) {
-        Integer deathsValue = this.deaths.get(player.getName());
+        Integer deathsValue = this.deaths.get(player.getUniqueId());
         if(deathsValue == null) {
-            this.deaths.put(player.getName(), 0);
+            this.deaths.put(player.getUniqueId(), 0);
             deathsValue = 0;
         }
-        this.deaths.put(player.getName(), deathsValue + 1 );
+        this.deaths.put(player.getUniqueId(), deathsValue + 1 );
     }
 
     public void incrementXp(Player player) {
-        Integer xpValue = this.xp.get(player.getName());
+        Integer xpValue = this.xp.get(player.getUniqueId());
         if(xpValue == null) {
-            this.xp.put(player.getName(), 0);
+            this.xp.put(player.getUniqueId(), 0);
             xpValue = 0;
         }
-        this.xp.put(player.getName(), xpValue + 1 );
+        this.xp.put(player.getUniqueId(), xpValue + 1 );
     }
 
     public int getKills(Player player) {
-        Integer killsValue = this.kills.get(player.getName());
+        Integer killsValue = this.kills.get(player.getUniqueId());
         if(killsValue == null) {
-            this.kills.put(player.getName(), 0);
+            this.kills.put(player.getUniqueId(), 0);
             killsValue = 0;
         }
         return killsValue;
     }
 
     public int getDeaths(Player player) {
-        Integer deathsValue = this.deaths.get(player.getName());
+        Integer deathsValue = this.deaths.get(player.getUniqueId());
         if(deathsValue == null) {
-            this.deaths.put(player.getName(), 0);
+            this.deaths.put(player.getUniqueId(), 0);
             deathsValue = 0;
         }
         return deathsValue;
     }
 
     public int getXp(Player player) {
-        Integer xpValue = this.xp.get(player.getName());
+        Integer xpValue = this.xp.get(player.getUniqueId());
         if(xpValue == null) {
-            this.xp.put(player.getName(), 0);
+            this.xp.put(player.getUniqueId(), 0);
             xpValue = 0;
         }
         return xpValue;
     }
 
-    public String getId(Player player) {
-        String idValue = this.id.get(player.getName());
+    public UUID getId(Player player) {
+        UUID idValue = this.id.get(player.getUniqueId());
         if (idValue == null) {
-            this.id.put(player.getName(), player.getName());
-            idValue = player.getName();
+            this.id.put(player.getUniqueId(), player.getUniqueId());
+            idValue = player.getUniqueId();
         }
         return idValue;
     }
