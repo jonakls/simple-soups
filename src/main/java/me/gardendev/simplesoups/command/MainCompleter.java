714f45110ee62351db48ad80c12a99cb822391e0
Jonakls <asmot54@hotmail.com>
2021-10-21 08:49:33 -0500

    Edited tab completer

--- src/main/java/me/gardendev/simplesoups/command/MainCompleter.java
+++ src/main/java/me/gardendev/simplesoups/command/MainCompleter.java
@@ -3,26 +3,32 @@ package me.gardendev.simplesoups.command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class MainCompleter implements TabCompleter {
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        List<String> list = new ArrayList<>();
+
+        if (!(sender instanceof Player)) {
+            return List.of("reload");
+        }
 
         if (args.length == 1){
 
-            list.add("help");
-            list.add("reload");
-            list.add("kits");
-            list.add("setspawn");
-            list.add("spawn");
-            list.add("editmode");
+            return List.of(
+                    "reload",
+                    "give",
+                    "save",
+                    "kits",
+                    "editmode",
+                    "setspawn",
+                    "spawn",
+                    "help"
+            );
         }
-
-        return list;
+        return null;
     }
 }
